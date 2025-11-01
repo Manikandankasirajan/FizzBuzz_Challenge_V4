40f6ee5ed655f8103a51d217bcb5a1fd8e432c3f
Manikandankasirajan <itsmanikandankasirajan@gmail.com>
2025-11-01 20:23:09 +0530

    chore : modify and group test cases for improve readability

--- src/tests/fizzBuzz.test.js
+++ src/tests/fizzBuzz.test.js
@@ -1,31 +1,42 @@
 import fizzBuzz from "../fizzbuzz";
 describe("test cases for FizzBuzz challenge", () => {
-	test("returns number if it is not multiple of 3 and 5", () => {
-		const num = 1;
-		expect(fizzBuzz(num)).toBe(num);
+	describe("test cases for standard inputs", () => {
+		test("returns number if it is not multiple of 3 and 5", () => {
+			const num = 1;
+			expect(fizzBuzz(num)).toBe(num);
+		});
+		test("returns Fizz for the multiples of 3", () => {
+			const num = 3;
+			expect(fizzBuzz(num)).toBe("Fizz");
+		});
+		test("returns Buzz for the multiples of 5", () => {
+			const num = 5;
+			expect(fizzBuzz(num)).toBe("Buzz");
+		});
+		test("returns FizzBuzz for the multiples of 3 and 5", () => {
+			const num = 15;
+			expect(fizzBuzz(num)).toBe("FizzBuzz");
+		});
 	});
-	test("returns Fizz for the multiples of 3", () => {
-		const num = 3;
-		expect(fizzBuzz(num)).toBe("Fizz");
+	describe("random numbers to validate program certainty on different inputs", () => {
+		test("returns number if it is not multiple of 3 and 5", () => {
+			const num = 7;
+			expect(fizzBuzz(num)).toBe(num);
+		});
+		test("returns Fizz for the multiples of 3", () => {
+			const num2 = 9;
+			expect(fizzBuzz(num2)).toBe("Fizz");
+		});
+		test("returns Buzz for the multiples of 5", () => {
+			const num3 = 20;
+			expect(fizzBuzz(num3)).toBe("Buzz");
+		});
+		test("returns FizzBuzz for the multiples of 3 and 5", () => {
+			const num4 = 30;
+			expect(fizzBuzz(num4)).toBe("FizzBuzz");
+		});
 	});
-	test("returns Buzz for the multiples of 5", () => {
-		const num = 5;
-		expect(fizzBuzz(num)).toBe("Buzz");
-	});
-	test("returns FizzBuzz for the multiples of 3 and 5", () => {
-		const num = 15;
-		expect(fizzBuzz(num)).toBe("FizzBuzz");
-	});
-	test("random numbers to check program certainty", () => {
-		const num = 7;
-		expect(fizzBuzz(num)).toBe(num);
-		const num2 = 9;
-		expect(fizzBuzz(num2)).toBe("Fizz");
-		const num3 = 20;
-		expect(fizzBuzz(num3)).toBe("Buzz");
-		const num4 = 30;
-		expect(fizzBuzz(num4)).toBe("FizzBuzz");
-	});
+
 	test("throw error for invalid input", () => {
 		const num = "ee";
 		expect(() => fizzBuzz(num)).toThrow();
